3841e76526aecb3135c229c96ab1f0ac862a8ba6
Surender Yerva <Surender Yerva>
2016-02-11 21:11:17 -0800

    Template Project

---
+++ CodeEval/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CodeEval.Solutions;
+
+namespace CodeEval
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Testing Sample Solution
+            TestSampleSolution();
+        }
+
+        static void TestSampleSolution()
+        {
+            SampleSolution.Print();
+        }
+    }
+}
